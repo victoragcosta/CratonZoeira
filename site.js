ba37d7415f1757c462ca4da3e5244235e07aa6e7
Victor André Gris Costa <victorandr98@gmail.com>
2020-05-10 21:44:12 -0300

    Made navbar responsive

--- site.js
+++ site.js
@@ -1,17 +1,66 @@
-// Fix Missing navbar height due to fixed position
-$("nav.navbar.fixed").each(function () {
-  let next = $(this).next();
-  next.css(
-    "padding-top",
-    `calc(${next.css("padding-top")} + ${$(this).height()}px)`
-  );
+let observer = new ResizeObserver(function (entries) {
+  for (let entry of entries) {
+    entry.target.handleResize(entry);
+  }
 });
 
-$(".expand-menu").click(function (ev) {
-  let ul = $(this).next("ul");
-  if (ul.css("display") === "flex") {
-    ul.css("display", "none");
-  } else {
-    ul.css("display", "flex");
+let toggleNavbarList = function (ev) {
+  let navbar = $(this).parent("nav.navbar");
+  let ul = $(this).nextAll("ul");
+
+  navbar.toggleClass("shrunk");
+  ul.slideToggle();
+};
+
+function initNavbar() {
+  let next = $("nav.navbar.fixed").next();
+  next[0].originalPaddingTop = next.css("padding-top");
+
+  $("nav.navbar.fixed").each(function () {
+    this.handleResize = function (entry) {
+      // Fix Missing navbar height due to fixed position
+      let next = $(this).next();
+      next.css(
+        "padding-top",
+        `calc(${next[0].originalPaddingTop} + ${$(this).height()}px)`
+      );
+    };
+
+    observer.observe(this, {
+      attributes: true,
+    });
+
+    // Hide lists
+    $(".expand-menu").click(toggleNavbarList);
+    let viewportWidth = window.innerWidth;
+    if (viewportWidth < 768 && $(this).hasClass("shrunk")) {
+      $(".expand-menu").click();
+      $(this).addClass("shrunk");
+    }
+  });
+
+  $(window).resize(function () {
+    $("nav.navbar").each(function () {
+      // Show list when width big enough and hide if necessary when width shrinks
+      let viewportWidth = window.innerWidth;
+      let ul = $(this).children("ul");
+      if (viewportWidth < 768) {
+        // vertical mode
+        console.log("aha2");
+        if ($(this).hasClass("shrunk")) {
+          console.log("aha");
+          ul.hide();
+        }
+      } else {
+        // horizontal mode
+        ul.show();
+      }
+    });
+  });
+}
+
+$(document).ready(function () {
+  if ($("nav.navbar.fixed")[0]) {
+    initNavbar();
   }
 });
